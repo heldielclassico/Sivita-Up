9a3341a3918919a3a256883f666bbe7da2c150ce
heldielclassico <113118436+heldielclassico@users.noreply.github.com>
2026-01-24 02:45:25 +0700

    Update app.py

--- app.py
+++ app.py
@@ -154,7 +154,7 @@ if st.session_state.vector_store is None:
 # --- 6. UI UTAMA ---
 
 # Judul Utama
-st.markdown("<h1 style='text-align: center; margin-top: -40px; margin-bottom: -25px;'>🎓 Asisten Virtual Poltesa (Sivita)</h1>", unsafe_allow_html=True)
+st.markdown("<h1 style='text-align: center; margin-top: -40px; margin-bottom: -23px;'>🎓 Asisten Virtual Poltesa (Sivita)</h1>", unsafe_allow_html=True)
 st.markdown("<p style='text-align: center; color: gray; margin-bottom: 15px;'>Sivita v1.3 | Modular Prompt System</p>", unsafe_allow_html=True)
 
 # --- BAGIAN INPUT ---
