0b4f4133ced95971296e4230ac8b4d4fc9d0631c
heldielclassico <113118436+heldielclassico@users.noreply.github.com>
2026-01-24 00:04:58 +0700

    Update app.py

--- app.py
+++ app.py
@@ -40,7 +40,7 @@ st.markdown(f"""
     /* STYLE UNTUK MEMBUAT AREA INPUT TETAP DI BAWAH (STICKY/FIXED) */
     div[data-testid="stVerticalBlock"] > div:has(div.floating-anchor) {{
         position: fixed;
-        bottom: 20px; /* Posisi panel dinaikkan dari dasar layar */
+        bottom: 10px; /* Posisi panel dinaikkan dari dasar layar */
         left: 50%;
         transform: translateX(-50%);
         width: 95%;
