dec0dc231f86a407817de957054735e1a6888418
heldielclassico <113118436+heldielclassico@users.noreply.github.com>
2026-01-24 02:57:54 +0700

    Update app.py

--- app.py
+++ app.py
@@ -255,5 +255,5 @@ with st.container(border=True):
                 placeholder_animasi.empty()
                 st.error(f"Error: {e}")
 
-st.divider()
+
 st.caption("Sivita - Virtual Assistant Poltesa @2026")
