d96d22d5a9a6ebb6e83408104d0a992cf60f0b6b
heldielclassico <113118436+heldielclassico@users.noreply.github.com>
2026-01-25 20:48:33 +0700

    Update app.py

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ st.markdown(f"""
     
     /* Menaikkan seluruh konten ke atas */
     .block-container {{
-        padding-top: 5px;
+        padding-top: 12px;
         padding-bottom: 0rem;
     }}
     
